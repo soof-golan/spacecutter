df80b013077058574100785cf6b15cf0ad8e790c
Soof Golan <soofgolan@gmail.com>
2023-02-27 22:37:59 +0400

    export common stuff to top of package

--- spacecutter/__init__.py
+++ spacecutter/__init__.py
@@ -1,1 +1,11 @@
+from .losses import CumulativeLinkLoss
+from .models import OrdinalLogisticHead
+from .callbacks import AscensionCallback
+
 __version__ = "0.3.1"
+
+__all__ = [
+    "CumulativeLinkLoss",
+    "OrdinalLogisticHead",
+    "AscensionCallback",
+]
