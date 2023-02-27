a8a8a9ebdd224e98048c901fdb5ded4577faeb7b
Soof Golan <soofgolan@gmail.com>
2023-02-27 22:38:13 +0400

    0.3.2

--- spacecutter/__init__.py
+++ spacecutter/__init__.py
@@ -2,7 +2,7 @@ from .losses import CumulativeLinkLoss
 from .models import OrdinalLogisticHead
 from .callbacks import AscensionCallback
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 __all__ = [
     "CumulativeLinkLoss",
